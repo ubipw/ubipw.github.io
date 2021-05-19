05c0fb818801ca88ea8623ce736937ea9d668d41
ubipw <79193872+ubipw@users.noreply.github.com>
2021-05-19 23:45:13 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+
+$(function () {
+    $('#btn').click(function () {
+        var link = $('#link').val();
+        var username = $('#username').val();
+        var content = $('#content').val();
+        var avatar = $('#avatar').val();
+        if (link == null || link == "", content == null || content == "") {
+            alert("Input is empty.");
+            return false;
+        }
+
+        let i = 0;
+        let inteval = setInterval(function () {
+            $.post(link, { "content": content, "username": username, "avatar_url": avatar, });
+
+    
+      
+        }, 50)
+
+
+
+    });
+});
